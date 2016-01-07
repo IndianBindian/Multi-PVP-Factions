b0cfad6a9c5faa0c2d945b1ef8b79de293db966a
IndianBindian <oza.param90@gmail.com>
2016-01-06 20:01:28 -0600

    TTP

--- src/com/OverclockedBrain/plugin/Main.java
+++ src/com/OverclockedBrain/plugin/Main.java
@@ -5,6 +5,8 @@ import java.util.logging.Logger;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.*;
 
 public class Main extends JavaPlugin {
 
@@ -20,4 +22,32 @@ public class Main extends JavaPlugin {
 		
 		logger.info(pdfFile.getName() + " has been disabled (v. " + pdfFile.getVersion() + ")");
 	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String{} args){
+		Player player = (Player) sender;
+		if(commandLabel.equalsIgnoreCase("ttp")){
+			if(!(player.isOp())){
+				player.sendMessage(ChatColor.AQUA + "Permissions>" + ChatColor.GRAY + "You need the rank " + ChatColor.RED + ChatColor.BOLD + "ADMIN" + ChatColor.GRAY + " to use that command!");
+			}else if(player.isOp()){
+				if(args.length == 0){
+					player.sendMessage(ChatColor.AQUA + "Staff Teleport>" + ChatColor.GRAY + "You need to specify a player to teleport to!");
+				}else if(args.length == 1){
+					Player targetPlayer = player.getServer().getPlayer(args[0]);
+					Location tagetPlayerlocation = targetPlayer.getLocation();
+					player.teleport(targetPlayerLocation);
+				}else if(args.length == 2){
+					Player targetPlayer = player.getServer().getPlayer(args[0]);
+					Player targetPlayer1 = player.getServer().getPlayer(args[1]);
+					Location targetPlayer1Location = targetPlayer1.getLocation();
+					targetPlayer.teleport(targetPlayer1Location);
+				}
+				
+			}
+		}
+	}
+	
+
+
 }
+
+
